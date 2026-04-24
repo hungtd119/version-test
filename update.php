01406cc066f960b2eb47f580948776f8425d92bc
hungtd <hungtd@appota.com>
2026-04-24 13:59:24 +0700

    test

---
+++ update.php
@@ -0,0 +1,22 @@
+<?php return array (
+  309 => 
+  array (
+    0 => 
+    array (
+      'url' => 'c2d91468c67061bc7ce64875cfa25da9.7z',
+      'size' => 57670767,
+      'md5' => 'c2d91468c67061bc7ce64875cfa25da9',
+      'from_version' => 309,
+    ),
+  ),
+  354 => 
+  array (
+    0 => 
+    array (
+      'url' => '57e30146a65bde3f6157a3d92b04ff80.7z',
+      'size' => 5286985,
+      'md5' => '57e30146a65bde3f6157a3d92b04ff80',
+      'from_version' => 354,
+    ),
+  ),
+);
